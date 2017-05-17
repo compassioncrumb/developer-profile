a83c90d45a4588aad27e9d0f7a6fe598eeceb722
compassionCrumb <compassioncrumb@gmail.com>
2017-05-17 16:39:45 -0600

    reorder projects

--- js/index.js
+++ js/index.js
@@ -1,54 +1,54 @@
 $( document ).ready(function() {
 
 var cardinals = {
-  alpha: {
+profile: {
+  title: "Public Profile",
+  summary: "stylized profile page for a speaker or public figure",
+  screenshot: "./screenshot_size/dennet_300x190.png",
+  url: "https://compassioncrumb.github.io/front-end-composition-1/",
+  skills: ["scss", "html5"]
+}, 
+jobBoard: {
+  title: "Modern Job Board", 
+  summary: "detailed layout for a modern job board",
+  screenshot: "./screenshot_size/job_board_300x190.png",
+  url: "https://compassioncrumb.github.io/job-board-composition/",
+  skills: ["scss", "html5", "flexbox"]
+},
+userInfo: {
+  title: "User Info Cards",
+  summary: "card-based layout to display profile information",
+  screenshot: "./screenshot_size/user_info_300x190.png",
+  url: "https://compassioncrumb.github.io/users-info-composition/",
+  skills: ["scss", "html"]
+},
+wikipedia: {
+  title: "Wikipedia Search",
+  summary: "utilizes the wikipedia api to fetch search results and tastefully display them",
+  screenshot: "./screenshot_size/wikipedia_300x190.png",
+  url: "https://compassioncrumb.github.io/wikipedia-search/",
+  skills: ["ajax", "jqeury", "javascript"]
+},
+pomodoro: {
+  title: "Pomodoro Timer", 
+  summary: "stay focused, then take a break with this minimalist pomodoro timer",
+  screenshot: "./screenshot_size/il_doro_300x190.png",
+  url: "https://compassioncrumb.github.io/il-doro/",
+  skills: ["jquery", "javascript", "transitions"]
+}, 
+simon: {
     title: "Simon Signifies", 
     summary: "a simple, elegant simon says game",
     screenshot: "./screenshot_size/simon_300x190.png",
     url: "https://compassioncrumb.github.io/simon-signifies/",
-    skills: ["jquery", "javascript", "scss"]
+    skills: ["jquery", "javascript"]
   },
-  beta: {
-    title: "Pomodoro Timer", 
-    summary: "stay focused, then take a break with this minimalist pomodoro timer",
-    screenshot: "./screenshot_size/il_doro_300x190.png",
-    url: "https://compassioncrumb.github.io/il-doro/",
-    skills: ["jquery", "javascript", "transitions"]
-  }, 
-  gamma: {
-    title: "Wikipedia Search",
-    summary: "utilizes the wikipedia api to fetch search results and tastefully display them",
-    screenshot: "./screenshot_size/wikipedia_300x190.png",
-    url: "https://compassioncrumb.github.io/wikipedia-search/",
-    skills: ["ajax", "jqeury", "javascript"]
-  },
-  delta: {
-    title: "Public Profile",
-    summary: "stylized profile page for a speaker or public figure",
-    screenshot: "./screenshot_size/dennet_300x190.png",
-    url: "https://compassioncrumb.github.io/front-end-composition-1/",
-    skills: ["jqeury", "javascript", "scss"]
-  }, 
-  epsilon: {
-    title: "Modern Job Board", 
-    summary: "detailed layout for a modern job board",
-    screenshot: "./screenshot_size/job_board_300x190.png",
-    url: "https://compassioncrumb.github.io/job-board-composition/",
-    skills: ["scss", "html", "javascript"]
-  },
-  phi: {
-    title: "User Info Cards",
-    summary: "card-based layout to display profile information",
-    screenshot: "./screenshot_size/user_info_300x190.png",
-    url: "https://compassioncrumb.github.io/users-info-composition/",
-    skills: ["scss", "html"]
-  },
-  iota: {
+  library: {
     title: "Library Catalog", 
     summary: "back-end and form based library catalog site, with content management functions",
     screenshot: "./screenshot_size/library_300x190.png", 
     url: "https://townlibrary.herokuapp.com/catalog/book/create",
-    skills: ["node", "pug/jade", "javascript"]
+    skills: ["node", "pug/jade", "Express"]
   }
 };
 
